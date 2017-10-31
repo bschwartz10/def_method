53d35083357af06be788cc8f60e79d6d37454398
bschwartz10 <bschwartz92@gmail.com>
2017-10-31 16:10:49 -0400

    dynamic writing of players

--- lib/players_writer.rb
+++ lib/players_writer.rb
@@ -2,7 +2,8 @@ class PlayersWriter
   def self.write_players(players)
     File.open('./output/target_output.txt', 'w') do |line|
       players.each do |player|
-        line << "#{player.last_name} #{player.first_name} #{player.gender} #{player.date_of_birth} #{player.favorite_color}\n"
+        player.attributes.map {|attribute, value| line << "#{value} "}
+        line << "\n"
       end
     end
   end
