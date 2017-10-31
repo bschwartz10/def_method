073f659b77897b7a055c43e11139fb0df928b9e5
bschwartz10 <bschwartz92@gmail.com>
2017-10-31 16:50:02 -0400

    delete all method(moved strip to reader class) and switch indexes of dob and fav color

--- lib/incoming_players.rb
+++ lib/incoming_players.rb
@@ -1,27 +1,31 @@
 class IncomingPlayers
-  attr_reader :all
+  attr_reader :players
 
   def initialize(players)
-    @all = players.map {|player| player.strip}
+    @players = players
   end
 
   def pipe_players
-    all.map do |player|
+    players.map do |player|
       np = player.gsub(/\s+/, "").tr('|',',').split(',')
       np.delete_at(2)
+      np[3], np[4] = np[4], np[3]
       Player.new(np)
     end
   end
 
   def comma_players
-    all.map {|player| Player.new(player.gsub(/\s+/, "").split(','))}
+    players.map do |player|
+      np = player.gsub(/\s+/, "").split(',')
+      np[3], np[4] = np[4], np[3]
+      Player.new(np)
+    end
   end
 
   def space_players
-    all.map do |player|
+    players.map do |player|
       np = player.split(' ')
       np.delete_at(2)
-      np[3], np[4] = np[4], np[3]
       Player.new(np)
     end
   end
