b7ea667fda84b13be9124d0e099d7569fda20f8c
bschwartz10 <bschwartz92@gmail.com>
2017-10-31 16:50:18 -0400

    methods are now dependent on attributes hash

--- lib/outgoing_players.rb
+++ lib/outgoing_players.rb
@@ -8,15 +8,15 @@ class OutgoingPlayers
   end
 
   def output_1
-    all.sort_by {|player| [player.gender, player.last_name]}
+    all.sort_by {|player| [player.attributes[:gender], player.attributes[:last_name]]}
   end
 
   def output_2
-    all.sort_by {|player| [Date.strptime(player.date_of_birth, '%m/%d/%Y'), player.last_name]}
+    all.sort_by {|player| [Date.strptime(player.attributes[:date_of_birth], '%m/%d/%Y'), player.last_name]}
   end
 
   def output_3
-    all.sort_by {|player| player.last_name }.reverse!
+    all.sort_by {|player| player.attributes[:last_name] }.reverse!
   end
 
 end
