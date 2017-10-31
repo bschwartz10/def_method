d0adf018b32aa625e1058c6525071e3f6afea1a4
bschwartz10 <bschwartz92@gmail.com>
2017-10-31 16:10:05 -0400

    WIP creating attributes hash to create key value pairs

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,36 @@
 class Player
-  attr_reader :player, :last_name, :first_name, :gender, :favorite_color, :date_of_birth
+  attr_reader :player, :attributes, :last_name, :first_name, :gender, :favorite_color, :date_of_birth
 
   def initialize(player)
     @player = player
-    @last_name = player[0]
-    @first_name = player[1]
-    @gender = formatted_gender
-    @favorite_color = player[3]
-    @date_of_birth = formatted_date_of_birth
+    @attributes = set_attributes
+    # @last_name = player[0]
+    # @first_name = player[1]
+    # @gender = formatted_gender
+    # @favorite_color = player[3]
+    # @date_of_birth = formatted_date_of_birth
+  end
+
+  def stock_attributes
+    [
+      :last_name,
+      :first_name,
+      :gender,
+      :favorite_color,
+      :date_of_birth,
+    ]
+  end
+
+  def set_attributes
+    Hash[stock_attributes.zip(player)]
   end
 
   def formatted_date_of_birth
-    player[4].tr('-', '/')
+    attributes[:date_of_birth].tr!('-', '/')
   end
 
   def formatted_gender
-    player[2].start_with?('M') ? 'Male' : 'Female'
+    attributes[:gender].start_with?('M') ? 'Male' : 'Female'
   end
 
 end
