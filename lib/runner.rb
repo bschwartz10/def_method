a197c2b08e39fc159f5056bf23fd84030b1c32f5
bschwartz10 <bschwartz92@gmail.com>
2017-10-31 16:51:30 -0400

    change from all method to players

--- lib/runner.rb
+++ lib/runner.rb
@@ -4,21 +4,20 @@ require './lib/players_writer'
 require './lib/outgoing_players'
 require './lib/players_reader'
 
-txt_files = ['./input/comma.txt', './input/pipe.txt', './input/space.txt']
+files = ['./input/comma.txt', './input/pipe.txt', './input/space.txt']
 
-raw_players = txt_files.map do |txt_file|
-  PlayersReader.read_players(txt_file)
+raw_players = files.map do |file|
+  PlayersReader.read_players(file)
 end
 
 players = raw_players.map do |incoming_players|
-  if incoming_players.all.first.include?(',')
+  if incoming_players.players.first.include?(',')
     incoming_players.comma_players
-  elsif incoming_players.all.first.include?('|')
+  elsif incoming_players.players.first.include?('|')
     incoming_players.pipe_players
   else
     incoming_players.space_players
   end
 end
-
 outgoing_players = OutgoingPlayers.new(players).output_3
 PlayersWriter.write_players(outgoing_players)
