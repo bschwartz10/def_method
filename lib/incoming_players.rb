6185a38cfb4615ac15169a30593580c6d02b3faa
bschwartz10 <bschwartz92@gmail.com>
2017-11-01 11:54:29 -0400

    WIP first run at normalize method

--- lib/incoming_players.rb
+++ lib/incoming_players.rb
@@ -1,12 +1,26 @@
 class IncomingPlayers
-  attr_reader :players
+  attr_reader :raw_players
 
-  def initialize(players)
-    @players = players
+  def initialize(raw_players)
+    @raw_players = raw_players.map {|raw_player| raw_player.gsub(/[,|]/, "").split(' ')}
   end
 
-  def pipe_players
-    players.map do |player|
+  def normalize
+    new_players = raw_players.map do |raw_player|
+      raw_player.delete_at(2) if raw_player.count == 6
+      raw_player
+    end
+    new_players.map do |np|
+      if np[4].include?('-') || np[4].include?('/')
+        np[3], np[4] = np[4], np[3]
+      end
+      Player.new(np)
+    end
+  end
+
+  def pipe_players #6
+    raw_players.map do |player|
+      require "pry"; binding.pry
       np = player.gsub(/\s+/, "").tr('|',',').split(',')
       np.delete_at(2)
       np[3], np[4] = np[4], np[3]
@@ -14,16 +28,16 @@ class IncomingPlayers
     end
   end
 
-  def comma_players
-    players.map do |player|
+  def comma_players #5
+    raw_players.map do |player|
       np = player.gsub(/\s+/, "").split(',')
       np[3], np[4] = np[4], np[3]
       Player.new(np)
     end
   end
 
-  def space_players
-    players.map do |player|
+  def space_players #6
+    raw_players.map do |player|
       np = player.split(' ')
       np.delete_at(2)
       Player.new(np)
